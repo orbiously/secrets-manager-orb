0ca3b21fa44ba8f3d76eed5957861b2a0b65ed5b
Yann <yann.domingo@gmail.com>
2023-01-19 19:46:48 +0000

    fix(orb scripts): using token variable from parameters

--- src/scripts/check.sh
+++ src/scripts/check.sh
@@ -26,7 +26,7 @@ esac
 
 if  ! curl -s "https://circleci.com/api/v2/me/collaborations" -H "Circle-Token: ${!PARAM_CIRCLE_TOKEN}" | jq -r '.[].name' | grep -q "${PARAM_ORG_NAME}"; then
   echo "Unable to confirm access to the organization."
-  echo -e "Either there is no $DISPLAY_VCS organization named \'$PARAM_ORG_NAME\' known to CircleCI,\n"
+  echo -e "Either there is no $DISPLAY_VCS organization named '$PARAM_ORG_NAME' known to CircleCI,\n"
   echo -e "or the user who owns this API token is not a member of the '$PARAM_ORG_NAME' organization in $DISPLAY_VCS."
   echo "Make sure you specified the correct name (with the exact capitulization) and that make sure the CircleCI personal API token is valid."
   exit 1
